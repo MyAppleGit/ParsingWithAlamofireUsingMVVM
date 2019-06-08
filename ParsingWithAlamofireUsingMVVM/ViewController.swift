80cbdcd10132640148037c43dc0cc9486a9d11c2
Avaz Kadiraliev <akadiraliev6@gmail.com>
2019-06-08 14:10:16 +0600

    Initial Commit

---
+++ ParsingWithAlamofireUsingMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParsingWithAlamofireUsingMVVM
+//
+//  Created by Avazbek Kodiraliev on 6/8/19.
+//  Copyright © 2019 Avazbek Kodiraliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
